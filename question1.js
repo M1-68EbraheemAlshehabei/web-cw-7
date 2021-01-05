6dd81364a9ccf8c7fbcbf3fb35e11fa9f994e867
M1-68EbraheemAlshehabei <76258901+M1-68EbraheemAlshehabei@users.noreply.github.com>
2021-01-05 21:16:15 +0300

    Done!

--- question1.js
+++ question1.js
@@ -1,1 +1,20 @@
 // قم بحل السؤال الأول هنا
+let pets = [`goat`, `cat`, `dog`,`sheep`,`kamel`];
+
+console.log(pets);
+console.log(pets.length);
+
+pets.pop();
+console.log(pets);
+
+pets.push(`brid`);
+console.log(pets);
+
+
+let cat = {
+    name: `Anood`, 
+    age: `3 months`,
+    species: `cat`,
+    emoji: `😸`,
+}
+console.log(cat.age);
